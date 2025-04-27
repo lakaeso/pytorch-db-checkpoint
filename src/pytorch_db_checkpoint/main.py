ca9e073c7ad9563820e43734465e35bbbe92e001
lakaeso <antonio.lakos1@gmail.com>
2025-04-27 12:37:28 +0200

    added _connection_decorator, refactoring

--- src/pytorch_db_checkpoint/main.py
+++ src/pytorch_db_checkpoint/main.py
@@ -33,105 +33,97 @@ class DBHandler:
         except (psycopg2.DatabaseError, Exception) as error:
             print(error)
     
-    def save_training_state(self, model_name: str, epoch: int, model: nn.Module, optim: torch.optim.Optimizer, metrics: dict = None, comment: str = None):
+    def _connection_decorator(func):
+        def wrapper(self, *args, **kwargs):
+            with self._create_connection() as conn:
+                with conn.cursor() as cur:
+                    kwargs["cur"] = cur
+                    func(self, *args, **kwargs)
+                    cur.close()
+        return wrapper
+    
+    @_connection_decorator
+    def save_training_state(self, model_name: str, epoch: int, model: nn.Module, optim: torch.optim.Optimizer, metrics: dict = None, comment: str = None, *args, **kwargs):
 
         # TODO: check if model with the same name exists but with larger epoch number
         # ask user to overwrite? or overwrite flag??
+
+        cur = kwargs["cur"]
 
         model_state_dict = pickle.dumps(model.state_dict())
 
         optim_state_dict = pickle.dumps(optim.state_dict())
 
         metrics_str = json.dumps(metrics)
-
-        with self._create_connection() as conn:
             
-            cur = conn.cursor()
-            
-            cur.execute(
-                """
-                INSERT INTO training_checkpoint 
-                        (epoch, model_name, model_state_dict, optim_state_dict, timestamp_inserted, comment, metrics) 
-                VALUES 
-                        (%s, %s, %s, %s, current_timestamp, %s, %s)
-                """, 
-                (epoch, model_name, model_state_dict, optim_state_dict, comment, metrics_str)
-            )
-            
-            conn.commit()
-            
-            cur.close()
-        
-        conn.close()
+        cur.execute(
+            """
+            INSERT INTO training_checkpoint 
+                    (epoch, model_name, model_state_dict, optim_state_dict, timestamp_inserted, comment, metrics) 
+            VALUES 
+                    (%s, %s, %s, %s, current_timestamp, %s, %s)
+            """, 
+            (epoch, model_name, model_state_dict, optim_state_dict, comment, metrics_str)
+        )
     
-    def load_training_state_last_epoch(self, model, optim, model_name):
+    @_connection_decorator
+    def load_training_state_last_epoch(self, model, optim, model_name, *args, **kwargs):
+
+        cur = kwargs["cur"]
         
-        with self._create_connection() as conn:
-        
-            cur = conn.cursor()
+        cur.execute(
+            """
+            SELECT 
+                * 
+            FROM 
+                training_checkpoint 
+            WHERE 
+                model_name = %s 
+            ORDER BY 
+                epoch DESC
+            """, 
+            (model_name, )
+        )
 
-            cur.execute(
-                """
-                SELECT 
-                    * 
-                FROM 
-                    training_checkpoint 
-                WHERE 
-                    model_name = %s 
-                ORDER BY 
-                    epoch DESC
-                """, 
-                (model_name, )
-            )
+        # TODO: add not found exception
 
-            # TODO: add not found exception
+        obj = cur.fetchone()
 
-            obj = cur.fetchone()
+        epoch = obj[1]
 
-            epoch = obj[1]
+        model.load_state_dict(pickle.loads(obj[3]))
 
-            model.load_state_dict(pickle.loads(obj[3]))
-
-            optim.load_state_dict(pickle.loads(obj[4]))
-
-            cur.close()
-
-        conn.close()
+        optim.load_state_dict(pickle.loads(obj[4]))
 
         return epoch, model, optim
     
-    def load_training_state_last_entry(self, model, optim, model_name):
-        
-        with self._create_connection() as conn:
-        
-            cur = conn.cursor()
+    @_connection_decorator
+    def load_training_state_last_entry(self, model, optim, model_name, *args, **kwargs):
 
-            cur.execute(
-                """
-                SELECT 
-                    * 
-                FROM 
-                    training_checkpoint 
-                WHERE 
-                    model_name = %s 
-                ORDER BY 
-                    timestamp_inserted DESC
-                """, 
-                (model_name, )
-            )
+        cur = kwargs["cur"]
+    
+        cur.execute(
+            """
+            SELECT 
+                * 
+            FROM 
+                training_checkpoint 
+            WHERE 
+                model_name = %s 
+            ORDER BY 
+                timestamp_inserted DESC
+            """, 
+            (model_name, )
+        )
 
-            # TODO: add not found exception
+        # TODO: add not found exception
 
-            obj = cur.fetchone()
+        obj = cur.fetchone()
 
-            epoch = obj[1]
+        epoch = obj[1]
 
-            model.load_state_dict(pickle.loads(obj[3]))
+        model.load_state_dict(pickle.loads(obj[3]))
 
-            optim.load_state_dict(pickle.loads(obj[4]))
-
-            cur.close()
-
-        conn.close()
+        optim.load_state_dict(pickle.loads(obj[4]))
 
         return epoch, model, optim
